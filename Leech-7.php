2f890065a6573ce94f2786e44cb3c190d531dbdc
Bradley Wogsland <bradley@wogsland.org>
2015-08-07 16:03:02 -0500

    trying to nail down all the failing cases...

--- Leech-7.php
+++ Leech-7.php
@@ -5,5 +5,78 @@ John Leech claims there are no sets of 7 distinct odd numbers the sum of whose r
 Note
 1/3 + 1/5 + 1/7 + 1/9 + 1/11 + 1/13 + 1/15 ~ 1.02
 so we can't immediately rule this possibility out.
+
+So how many sets sum to >= 1?
+
+1/3 + 1/5 + 1/7 + 1/9 + 1/11 + 1/13 + 1/17 ~ 1.01
+1/3 + 1/5 + 1/7 + 1/9 + 1/11 + 1/13 + 1/19 ~ 1.01
+1/3 + 1/5 + 1/7 + 1/9 + 1/11 + 1/13 + 1/21 > 1
+1/3 + 1/5 + 1/7 + 1/9 + 1/11 + 1/13 + 1/23 < 1
+1/3 + 1/5 + 1/7 + 1/9 + 1/11 + 1/15 + 1/17 > 1
+1/3 + 1/5 + 1/7 + 1/9 + 1/11 + 1/15 + 1/19 < 1
+1/3 + 1/5 + 1/7 + 1/9 + 1/11 + 1/17 + 1/19 < 1
+
+5 sets fulfill this relaxed criteria.
+
+A more general calculation:
 */
+$a = 3;
+$b = 5;
+$c = 7;
+$d = 9;
+$e = 11;
+$f = 13;
+$g = 15;
+$count = 0;
+while (1/$a + 1/$b + 1/$c + 1/$d + 1/$e + 1/$f + 1/$g >= 1) {
+  echo "in a loop\n";
+  while (1/$a + 1/$b + 1/$c + 1/$d + 1/$e + 1/$f + 1/$g >= 1) {
+    echo "in b loop\n";
+    while (1/$a + 1/$b + 1/$c + 1/$d + 1/$e + 1/$f + 1/$g >= 1) {
+      echo "in c loop\n";
+      while (1/$a + 1/$b + 1/$c + 1/$d + 1/$e + 1/$f + 1/$g >= 1) {
+        echo "in d loop\n";
+        while (1/$a + 1/$b + 1/$c + 1/$d + 1/$e + 1/$f + 1/$g >= 1) {
+          echo "in e loop\n";
+          while (1/$a + 1/$b + 1/$c + 1/$d + 1/$e + 1/$f + 1/$g >= 1) {
+            echo "in f loop\n";
+            while (1/$a + 1/$b + 1/$c + 1/$d + 1/$e + 1/$f + 1/$g >= 1) {
+              echo "($a,$b,$c,$d,$e,$f,$g)\n";
+              $count++;
+              $g = $g + 2;
+            }
+            $f = $f + 2;
+            $g = $f + 2;
+          }
+          $e = $e + 2;
+          $f = $e + 2;
+          $g = $f + 2;
+        }
+        $d = $d + 2;
+        $e = $d + 2;
+        $f = $e + 2;
+        $g = $f + 2;
+      }
+      $c = $c + 2;
+      $d = $c + 2;
+      $e = $d + 2;
+      $f = $e + 2;
+      $g = $f + 2;
+    }
+    $b = $b + 2;
+    $c = $b + 2;
+    $d = $c + 2;
+    $e = $d + 2;
+    $f = $e + 2;
+    $g = $f + 2;
+  }
+  $a = $a + 2;
+  $b = $a + 2;
+  $c = $b + 2;
+  $d = $c + 2;
+  $e = $d + 2;
+  $f = $e + 2;
+  $g = $f + 2;
+}
+echo "Found $count sets under the inequality.\n";
 ?>
--- Leech-9.php
+++ Leech-9.php
@@ -18,30 +18,56 @@ So it seems like the problem is actually finding 3 numbers a, b and c s.t.
 */
 $start = time();
 $a_start = 17;
-$a_range = 1000;
-$b_start = 17;
+$a_range = 100;
 $b_range = 1000;
-$c_start = 17;
-$c_range = 100000;
+$c_range = 10000000;
 for ($a=$a_start; $a<$a_range; $a = $a+2) {
+  //echo "trying $a\n";
   for ($b=$a+2; $b<$b_range; $b = $b+2) {
     for ($c=$b+2; $c<$c_range; $c = $c+2) {
       //echo "checking triple ($a,$b,$c)\n";
-      if ($a < $b && $b < $c) {
-        // This naive check misses one of the Leech-9s under 1000
-        //if (1/3 + 1/5 + 1/7 + 1/9 + 1/11 + 1/15 + 1/$a + 1/$b + 1/$c == 1) {
-        //
-        // Note 15*11*9*7*5*3 = 155925
-        $product = $a*$b*$c*155925;
-        if ($product/3 + $product/5 + $product/7 + $product/9 + $product/11 + $product/15 + $product/$a + $product/$b + $product/$c == $product) {
-          echo "A Leech-9 has been discovered: 3,5,7,9,11,15,$a,$b,$c\n";
-        }
+      // This naive check misses one of the Leech-9s under 1000
+      //if (1/3 + 1/5 + 1/7 + 1/9 + 1/11 + 1/15 + 1/$a + 1/$b + 1/$c == 1) {
+      //
+      // Note 15*11*9*7*5*3 = 155925
+      $product = $a*$b*$c*155925;
+      if ($product/3 + $product/5 + $product/7 + $product/9 + $product/11 + $product/15 + $product/$a + $product/$b + $product/$c == $product) {
+        echo "A Leech-9 has been discovered: 3,5,7,9,11,15,$a,$b,$c\n";
       }
     }
   }
 }
-$checked = round((($a_range-$a_start)/2)*(($b_range-$b_start)/2)*(($c_range-$c_start)/2));
+$checked = round((($a_range-$a_start)/2)*(($b_range-$a_start-2)/2)*(($c_range-$a_start-4)/2));
 $stop = time();
 $elasped = $stop - $start;
 echo "Checked roughly $checked odd number triples in $elasped seconds.\n"
+
+/*
+Explorations:
+(1000,1000,1000)
+A Leech-9 has been discovered: 3,5,7,9,11,15,21,165,693
+A Leech-9 has been discovered: 3,5,7,9,11,15,21,231,315
+A Leech-9 has been discovered: 3,5,7,9,11,15,33,45,385
+A Leech-9 has been discovered: 3,5,7,9,11,15,35,45,231
+Checked 118732760.875 odd number triples in 9 seconds.
+
+(1000,1000,100000)
+A Leech-9 has been discovered: 3,5,7,9,11,15,21,135,10395
+A Leech-9 has been discovered: 3,5,7,9,11,15,21,165,693
+A Leech-9 has been discovered: 3,5,7,9,11,15,21,231,315
+A Leech-9 has been discovered: 3,5,7,9,11,15,33,45,385
+A Leech-9 has been discovered: 3,5,7,9,11,15,35,45,231
+Checked 12076559135.875 odd number triples in 2823 seconds.
+
+(100,1000,100000)
+A Leech-9 has been discovered: 3,5,7,9,11,15,21,135,10395
+A Leech-9 has been discovered: 3,5,7,9,11,15,21,165,693
+A Leech-9 has been discovered: 3,5,7,9,11,15,21,231,315
+A Leech-9 has been discovered: 3,5,7,9,11,15,33,45,385
+A Leech-9 has been discovered: 3,5,7,9,11,15,35,45,231
+Checked roughly 1017573765 odd number triples in 433 seconds.
+
+(100,1000,10000000)
+
+*/
 ?>
